9e02ef1702a25f59261633ae76378c3362fb7b2f
Eirik Skjaerseth <eirik1502@gmail.com>
2018-12-25 21:09:57 +0100

    set 'run' task to run the dev game

--- src/main/java/game/CharacterAbilityUtils.java
+++ src/main/java/game/CharacterAbilityUtils.java
@@ -3,6 +3,7 @@ package game;
 import engine.WorldContainer;
 import engine.audio.Sound;
 import engine.combat.abilities.Ability;
+import engine.combat.abilities.AbilityComp;
 import engine.combat.abilities.MeleeAbility;
 import engine.combat.abilities.ProjectileAbility;
 import engine.physics.Circle;
@@ -18,10 +19,10 @@ import java.util.List;
 public class CharacterAbilityUtils {
 
 
-    public static List<Ability> parseApilities(WorldContainer wc, List<AbilityAttribs> abilitiesAttribs) {
-        return null;
-    }
-    public static Ability parseApility(WorldContainer wc, AbilityAttribs abilityAttrib) {
+//    public static List<Ability> parseApilities(WorldContainer wc, List<AbilityAttribs> abilitiesAttribs) {
+//        return null;
+//    }
+    public static Ability addAbility(WorldContainer wc, int entity, AbilityAttribs abilityAttrib) {
 
         if (abilityAttrib instanceof MeleeAbilityAttribs) {
 
@@ -43,4 +44,20 @@ public class CharacterAbilityUtils {
         return null;
     }
 
+    private void addMeleeAbility(WorldContainer wc, int entity, MeleeAbilityAttribs abAttribs, boolean media) {
+//        Sound sndBoom = null;
+//        Sound sndHit = null;
+//
+//        if (media) {
+//            sndBoom = new Sound("audio/boom-bang.ogg");
+//            sndHit = new Sound("audio/laser_hit.ogg");
+//        }
+//
+//        //puffer
+//        int melee1Entity = MeleeAbilityUtils.allocateHitboxEntity(wc, new Circle(98f), sndBoom, media); //old rad = 128. Old base knockback = 900
+//        MeleeAbility abPuffer = new MeleeAbility(wc, pufferAbilityIndex, melee1Entity, 8, 2, 8, 60*3, 0f);
+//        abPuffer.setDamagerValues(wc, 20, 1200f, 0.1f, 0f, false);
+//
+//        wc.addComponent(entity, new AbilityComp(abRapidshot, abHyperbeam, abPuffer));
+    }
 }
