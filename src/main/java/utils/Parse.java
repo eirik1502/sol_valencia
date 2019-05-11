2d172a03c8e2b1fd56acd9c7a07a2792b3234932
eirik1502 <eirik1502@gmail.com>
2019-05-11 13:20:04 +0200

    add string and parse utilities

---
+++ src/main/java/utils/Parse.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Parse {
+
+    public static int boolToInt(boolean b) {
+        return b? 1 : 0;
+    }
+
+    public static char boolToBinChar(boolean b) {
+        return b? '1' : '0';
+    }
+}
---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class StringUtils {
+
+    public static String removeEdges(String s) {
+        return s.substring(1, s.length()-1);
+    }
+}
